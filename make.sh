b3f165db07563bb87baa2ea37ce8ec18a607677b
Siavash <smirarab@gmail.com>
2012-12-30 19:21:23 -0600

    auto estimation of rates

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=2.2.3
+version=2.2.4
 cd main
 rm phylonet/coalescent/*.class phylonet/util/BitSet.class phylonet/tree/model/sti/STITreeCluster*.class 
 javac -classpath ../main.jar:../lib/jsr166.jar phylonet/util/BitSet.java phylonet/coalescent/*ClusterCollection*java phylonet/coalescent/DuplicationWeightCounter.java phylonet/util/BitSet.java phylonet/coalescent/ComputeMinCostTask.java phylonet/coalescent/MGDInference_DP.java phylonet/coalescent/CannotResolveException.java phylonet/tree/model/sti/STITreeCluster.java phylonet/coalescent/DeepCoalescencesCounter.java phylonet/coalescent/STBipartition.java
