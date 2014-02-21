04ac5c357c73487e6818446fda3921c5b88f97d7
Siavash Mirarab <toranj>
2014-02-21 16:14:38 -0600

    first pass at WQ

--- main/phylonet/coalescent/ComputeMinCostTask.java
+++ main/phylonet/coalescent/ComputeMinCostTask.java
@@ -2,26 +2,24 @@ package phylonet.coalescent;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import phylonet.coalescent.DuplicationWeightCounter.CalculateWeightTask;
+import phylonet.coalescent.Counter.CalculateWeightTask;
 import phylonet.tree.model.Tree;
 import phylonet.tree.model.sti.STITreeCluster;
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
-import phylonet.util.BitSet;
 
-public class ComputeMinCostTask {
+public abstract class ComputeMinCostTask<T> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 244989909835073096L;
-	private MGDInference_DP inference;
-	private Vertex v;
-	private ClusterCollection clusters;
+	DPInference<T> inference;
+	Vertex v;
+	ClusterCollection clusters;
 
 	protected Double compute() {
 		try {
@@ -31,7 +29,8 @@ public class ComputeMinCostTask {
 		}
 	}
 
-	public ComputeMinCostTask(MGDInference_DP inference, Vertex v, ClusterCollection clusters) {
+	public ComputeMinCostTask(DPInference<T> inference, Vertex v, 
+			ClusterCollection clusters) {
 		this.inference = inference;
 		this.v = v;
 		this.clusters = clusters;
@@ -40,7 +39,7 @@ public class ComputeMinCostTask {
 	// final int maxEL = 10000000;
 	ClusterCollection containedVertecies;
 
-	private void add_complementary_clusters(int clusterSize) {
+	private void addComplementaryClusters(int clusterSize) {
 		Iterator<Set<Vertex>> it = containedVertecies.getSubClusters().iterator();
 		while (it.hasNext()) {
 			Collection<Vertex> subClusters = new ArrayList<Vertex>(it.next());
@@ -52,7 +51,7 @@ public class ComputeMinCostTask {
 						getCompleteryVertx(x, v.getCluster()),
 						complementarySize);
 			}
-			if (i < clusterSize * inference.CD) {
+			if (i < clusterSize * inference.getCD()) {
 				return;
 			}
 
@@ -63,7 +62,7 @@ public class ComputeMinCostTask {
 
 		boolean rooted = inference.rooted;
 		List<Tree> trees = inference.trees;
-		DuplicationWeightCounter counter = inference.counter;
+		Counter<T> counter = inference.counter;
 
 		// -2 is used to indicate it cannot be resolved
 		if (v._done == 2) {
@@ -79,30 +78,13 @@ public class ComputeMinCostTask {
 
 		// SIA: base case for singelton clusters.
 		if (clusterSize <= 1) {
-			int _el_num = -1;
-			if (inference.optimizeDuploss == 3) {
-				if (GlobalMaps.taxonNameMap == null) {
-					_el_num = DeepCoalescencesCounter.getClusterCoalNum(trees,
-							v.getCluster(), rooted);
-					// System.out.println(v + " XL is " + _el_num);
-				} else {
-					_el_num = DeepCoalescencesCounter.getClusterCoalNumMap(trees,
-							v.getCluster(), rooted);
-				}
-			} else {
-				_el_num = 0;
-			}
-
-			// v._min_cost = 0;
-			v._max_score = -_el_num;
+			
+			v._max_score = scoreBaseCase(rooted, trees);
+			
 			v._min_lc = (v._min_rc = null);
 			v._done = 1;
 			return v._max_score;
 		}
-
-		List<Integer> El = new ArrayList<Integer>();
-		for (int k = 0; k < trees.size(); k++)
-			El.add(null);
 
 		boolean tryAnotherTime = false;
 
@@ -111,15 +93,15 @@ public class ComputeMinCostTask {
 		do {
 			tryAnotherTime = false;
 
-			if (clusterSize >= inference.counter.stTaxa.length * inference.CS) {
-				add_complementary_clusters(clusterSize);
+			if (clusterSize >= GlobalMaps.taxonIdentifier.taxonCount() * inference.getCS()) {
+				addComplementaryClusters(clusterSize);
 			}
 			Collection<STBipartition> clusterResolutions = containedVertecies
 					.getClusterResolutions();
-			int clusterDLCost = 0;
-			if (inference.optimizeDuploss == 3 && !clusterResolutions.isEmpty()) {
-				clusterDLCost = inference.counter.calculateDLstdClusterCost(
-						this.v.getCluster(), inference.trees);
+			
+			int clusterLevelCost = 0;
+			if (!clusterResolutions.isEmpty()) {
+				clusterLevelCost = calculateClusterLevelCost();
 			}
 			/*
 			 * System.out.println("xL: "+this.v.getCluster() + " "+xl + " "+
@@ -131,10 +113,10 @@ public class ComputeMinCostTask {
 				try {
 					Vertex smallV = containedVertecies.getVertexForCluster(bi.cluster1);
 					Vertex bigv = containedVertecies.getVertexForCluster(bi.cluster2);
-					ComputeMinCostTask smallWork = new ComputeMinCostTask(
-							inference, smallV, containedVertecies);
-					ComputeMinCostTask bigWork = new ComputeMinCostTask(
-							inference, bigv, containedVertecies);
+					ComputeMinCostTask<T> smallWork = newMinCostTask(
+							smallV, containedVertecies);
+					ComputeMinCostTask<T> bigWork = newMinCostTask(
+							bigv, containedVertecies);
 					CalculateWeightTask weigthWork = null;
 
 					// MP_VERSION: smallWork.fork();
@@ -158,45 +140,16 @@ public class ComputeMinCostTask {
 					}
 					// MP_VERSION: w = weigthWork.join();
 
-					Integer Wdom = counter.getCalculatedBiPartitionDPWeight(bi);
+					Integer Wdom = counter.getCalculatedWeight(STB2T(bi));
 
 					if (Wdom == null) {
-						weigthWork = counter.new CalculateWeightTask(bi,
-								containedVertecies);
+						weigthWork = counter.getWeightCalculateTask(STB2T(bi));
+						initializeWeightTask(weigthWork);
 						// MP_VERSION: smallWork.fork();
 						Wdom = weigthWork.compute();
 					}
-
-					Integer e = 0;
-					double c;
-					if (inference.optimizeDuploss == 3) {
-						
-						int OverlappingGeneCount = 0;
-						int someSideMissingXLCount = 0;
-						int bothSidesPresentGeneCount = 0;
-						for (int k = 0; k < inference.trees.size(); k++) {
-							STITreeCluster treeAll = inference.counter.treeAlls.get(k);
-							Tree tree = inference.trees.get(k);
-							boolean pDisJoint = smallV.getCluster().isDisjoint(treeAll);
-							boolean qDisJoint = bigv.getCluster().isDisjoint(treeAll);
-							if (pDisJoint || qDisJoint) {
-								someSideMissingXLCount +=  GlobalMaps.taxonNameMap == null ?
-									DeepCoalescencesCounter.getClusterCoalNum_rooted(tree, this.v.getCluster()):
-									DeepCoalescencesCounter.getClusterCoalNum_rootedMap(tree, this.v.getCluster());
-							}
-							if (!pDisJoint && !qDisJoint) {
-								bothSidesPresentGeneCount += 1;
-							}
-							if (!pDisJoint || !qDisJoint) {
-								OverlappingGeneCount += 1;
-							}							
-						}
-						
-						c = - ( clusterDLCost - 3 * Wdom - OverlappingGeneCount - someSideMissingXLCount 
-								+ inference.DLbdWeigth * (someSideMissingXLCount + OverlappingGeneCount + bothSidesPresentGeneCount) );
-					} else {
-						c = Wdom;
-					}					
+					
+					double c = adjustWeight(clusterLevelCost, smallV, bigv, Wdom);					
 
 					if ((v._max_score != -1)
 							&& (lscore + rscore + c < v._max_score)) {
@@ -214,16 +167,13 @@ public class ComputeMinCostTask {
 			}
 			if (v._min_lc == null || v._min_rc == null) {
 				if (clusterSize <= 5) {
-					addAllPossibleSubClusters(v.getCluster(),
-							containedVertecies);
+					addAllPossibleSubClusters(v.getCluster(), containedVertecies);
 					tryAnotherTime = true;
 				} else if (clusterSize > 1) {
 					// System.err.println(maxSubClusters);
-					Iterator<Set<Vertex>> it = containedVertecies
-							.getSubClusters().iterator();
+					Iterator<Set<Vertex>> it = containedVertecies.getSubClusters().iterator();
 					if (it.hasNext()) {
-						Collection<Vertex> biggestSubClusters = new ArrayList<Vertex>(
-								it.next());
+						Collection<Vertex> biggestSubClusters = new ArrayList<Vertex>(it.next());
 						int i = -1;
 						for (Vertex x : biggestSubClusters) {
 							i = i > 0 ? i : x.getCluster().getClusterSize();
@@ -249,7 +199,7 @@ public class ComputeMinCostTask {
 
 
 		if (v._min_lc == null || v._min_rc == null) {
-			if (MGDInference_DP._print) {
+			if (CommandLine._print) {
 				System.err.println("WARN: No Resolution found for ( "
 						+ v.getCluster().getClusterSize() + " taxa ):\n"
 						+ v.getCluster());
@@ -268,22 +218,19 @@ public class ComputeMinCostTask {
 		return v._max_score;
 	}
 
-	int calculateDLbdAdjustment(Vertex smallV, Vertex bigv) {
-		int e = 0;
-		for (int k = 0; k < inference.trees.size(); k++) {
-			STITreeCluster treeAll = inference.counter.treeAlls.get(k);
-			boolean pDisJoint = smallV.getCluster().isDisjoint(treeAll);
-			boolean qDisJoint = bigv.getCluster().isDisjoint(treeAll);
-			if (!pDisJoint && !qDisJoint) {
-				e += 1;
-			}
-			// System.err.println("E for " + v.getCluster() + " is "+e +
-			// " and k is  " + k);
-			// System.out.println(bigv + "|" + smallV+ " Total is " + e +
-			// " extra is "+extraTerms + " for tree" +k);
-		}
-		return e;
-	}
+	protected abstract ComputeMinCostTask<T> newMinCostTask(Vertex v, 
+	ClusterCollection clusters);
+
+	abstract protected double adjustWeight(int clusterLevelCost, Vertex smallV,
+			Vertex bigv, Integer Wdom);
+
+	abstract protected void initializeWeightTask(CalculateWeightTask weigthWork);
+
+    abstract protected int calculateClusterLevelCost();
+	
+	abstract protected int scoreBaseCase(boolean rooted, List<Tree> trees);
+	
+	abstract protected T STB2T(STBipartition stb);
 
 	void addAllPossibleSubClusters(STITreeCluster cluster,
 			ClusterCollection containedVertecies) {
