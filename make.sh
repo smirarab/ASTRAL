1d27d2bfbd6ec0547d6dfb725cbda11a6c4f0fb5
Siavash Mirarab <toranj>
2014-02-24 16:57:37 -0600

    fixed main

--- make.sh
+++ make.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-version=2.3.2
+version=3.0.0
+
 cd main
+
 rm phylonet/coalescent/*.class phylonet/util/BitSet.class phylonet/tree/model/sti/STITreeCluster*.class 
-javac -classpath ../main.jar:../lib/jsr166.jar phylonet/util/BitSet.java phylonet/coalescent/*ClusterCollection*java phylonet/coalescent/DuplicationWeightCounter.java phylonet/util/BitSet.java phylonet/coalescent/ComputeMinCostTask.java phylonet/coalescent/MGDInference_DP.java phylonet/coalescent/CannotResolveException.java phylonet/tree/model/sti/STITreeCluster.java phylonet/coalescent/DeepCoalescencesCounter.java phylonet/coalescent/STBipartition.java
-jar cvfm ../mgd.$version.jar ../manifest.text phylonet/util/BitSet.* phylonet/coalescent/DuplicationWeightCounter*.* phylonet/coalescent/*ClusterCollection* phylonet/coalescent/MGDInference_DP*.* phylonet/coalescent/CannotResolveException.* phylonet/tree/model/sti/STITreeCluster*.class phylonet/tree/model/sti/STITreeCluster.java phylonet/coalescent/DeepCoalescencesCounter.* phylonet/coalescent/ComputeMinCostTask.* phylonet/coalescent/STBipartition.*
+
+javac -classpath ../main.jar:../lib/jsr166.jar phylonet/util/BitSet.java phylonet/coalescent/*java phylonet/util/BitSet.java phylonet/tree/model/sti/STITreeCluster.java 
+
+jar cvfm ../astral.$version.jar ../manifest.text phylonet/util/BitSet.* phylonet/coalescent/*.* phylonet/tree/model/sti/STITreeCluster*.class phylonet/tree/model/sti/STITreeCluster.jav
+
 cd ..
-chmod +x mgd.$version.jar
-cp mgd.$version.jar mgd.jar
-sed -e "s/__mgd.jar__/mgd.$version.jar/g" README.template > README
-rm -r DynaDup/*
-mkdir DynaDup
-cd DynaDup
+
+chmod +x astral.$version.jar
+cp astral.$version.jar astral.jar
+sed -e "s/__astral.jar__/astral.$version.jar/g" README.template > README
+rm -r Astral/*
+mkdir Astral
+cd Astral
 ln -s ../lib .
 ln -s ../main.jar .
 ln -s ../README .
-ln -s ../mgd.$version.jar .
+ln -s ../astral.$version.jar .
 cd ..
-zip -r DynaDup.$version.zip DynaDup 
+zip -r Astral.$version.zip Astral 
