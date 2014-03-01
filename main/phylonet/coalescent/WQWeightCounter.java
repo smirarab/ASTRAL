b56dda6edda888b0be762212c12fa2018b9b4db6
Siavash Mirarab <toranj>
2014-03-01 09:21:38 -0600

    remove unused imports; increment version

--- main/phylonet/coalescent/WQWeightCounter.java
+++ main/phylonet/coalescent/WQWeightCounter.java
@@ -1,11 +1,9 @@
 package phylonet.coalescent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import phylonet.tree.model.TNode;
 import phylonet.tree.model.Tree;
