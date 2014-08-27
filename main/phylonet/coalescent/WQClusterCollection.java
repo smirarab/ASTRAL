fecb94c1e6e94317f17330c9da7168e991d26c9d
Siavash Mirarab <smirarab@gmail.com>
2014-08-27 17:14:14 -0500

    initial changes for 1000 taxon

--- main/phylonet/coalescent/WQClusterCollection.java
+++ main/phylonet/coalescent/WQClusterCollection.java
@@ -6,11 +6,16 @@ import phylonet.tree.model.sti.STITreeCluster;
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
 
 
-public class WQClusterCollection extends BasicClusterCollection{
+public class WQClusterCollection extends AbstractClusterCollection{
 
 	public WQClusterCollection(int len,  HashMap<STITreeCluster, Vertex> globalVertexCash) {
 		initialize(len, globalVertexCash);
 		//geneTreeSTBByCluster = new HashMap<STITreeCluster, HashSet<STBipartition>>();
+	}
+
+	@Override
+	public AbstractClusterCollection newInstance(int size) {
+		return new WQClusterCollection(size, this.globalVertexCash);
 	}
 		
 
