1ba6b45b1327e94cc5fc952f6b5536a9205e3f18
Siavash Mirarab <smirarab@gmail.com>
2015-04-28 09:52:54 -0500

    4.7.8 BUG fix: normalization of quartet score

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -38,7 +38,7 @@ import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
 
-    protected static String _versinon = "4.7.7";
+    protected static String _versinon = "4.7.8";
 
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
