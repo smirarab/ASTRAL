28bc3b9c4fa12234d0686f93b447ab85c3c8b479
Siavash Mirarab <smirarab@gmail.com>
2015-11-25 20:56:38 -0800

    first try at giving branch length and quartet support

--- main/phylonet/coalescent/WQInference.java
+++ main/phylonet/coalescent/WQInference.java
@@ -2,11 +2,17 @@ package phylonet.coalescent;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
+import phylonet.coalescent.BipartitionWeightCalculator.Quadrapartition;
+import phylonet.coalescent.WQWeightCalculator.QuartetWeightTask.Intersects;
 import phylonet.tree.model.TNode;
 import phylonet.tree.model.Tree;
+import phylonet.tree.model.sti.STINode;
+import phylonet.tree.model.sti.STITree;
 import phylonet.tree.model.sti.STITreeCluster;
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
 import phylonet.util.BitSet;
@@ -24,6 +30,7 @@ public class WQInference extends AbstractInference<Tripartition> {
 		this.forceAlg = alg;
 	}
 
+	
 	public void scoreGeneTree(Tree st) {
 
 		mapNames();
@@ -44,6 +51,7 @@ public class WQInference extends AbstractInference<Tripartition> {
 		
 		Stack<STITreeCluster> stack = new Stack<STITreeCluster>();
 		long sum = 0l;
+		long maxsum = 0l;
 		for (TNode node: st.postTraverse()) {
 			if (node.isLeaf()) {
 				String nodeName = node.getName(); //GlobalMaps.TaxonNameMap.getSpeciesName(node.getName());
@@ -77,9 +85,12 @@ public class WQInference extends AbstractInference<Tripartition> {
 				for (int i = 0; i < childbslist.size(); i++) {
 					for (int j = i+1; j < childbslist.size(); j++) {
 						for (int k = j+1; k < childbslist.size(); k++) {
-							sum += weightCalculator.getWeight(
-									new Tripartition(childbslist.get(i),  childbslist.get(j), childbslist.get(k))
-									, null);
+							Tripartition trip = new Tripartition(childbslist.get(i),  childbslist.get(j), childbslist.get(k));
+							Long s = weightCalculator.getWeight(trip, null);
+							sum += s;
+							//Long m = this.dataCollection.maxPossibleScore(trip);
+							//((STINode)node).setData(s*100l/m);
+							//maxsum += m;
 						}
 					}					       
 				}
@@ -91,8 +102,108 @@ public class WQInference extends AbstractInference<Tripartition> {
 				}
 			}
 		}
+/*		if (4l*this.maxpossible != maxsum) {
+			throw new RuntimeException("Hmm... "+maxsum+" "+4l*this.maxpossible);
+		}*/
 		System.out.println("Quartet score is: " + sum/4l);
 		System.out.println("Normalized quartet score is: "+ (sum/4l+0.)/this.maxpossible);
+		this.scoreGeneTree2(st);
+		System.out.println(st.toNewickWD());
+	}
+
+	
+	public void scoreGeneTree2(Tree st) {
+
+		weightCalculator = new BipartitionWeightCalculator(this);
+		BipartitionWeightCalculator weightCalculator2 = (BipartitionWeightCalculator) weightCalculator;
+		WQDataCollection wqDataCollection = (WQDataCollection) this.dataCollection;
+		//wqDataCollection.initializeWeightCalculator(this);
+		
+		Stack<STITreeCluster> stack = new Stack<STITreeCluster>();
+		for (TNode n: st.postTraverse()) {
+			STINode node = (STINode) n;
+			if (node.isLeaf()) {
+				String nodeName = node.getName(); //GlobalMaps.TaxonNameMap.getSpeciesName(node.getName());
+
+				STITreeCluster cluster = new STITreeCluster();
+				Integer taxonID = GlobalMaps.taxonIdentifier.taxonId(nodeName);
+				cluster.addLeaf(taxonID);
+
+				stack.add(cluster);
+				node.setData(cluster);
+
+			} else {
+				ArrayList<STITreeCluster> childbslist = new ArrayList<STITreeCluster>();
+				BitSet bs = new BitSet(GlobalMaps.taxonIdentifier.taxonCount());
+				for (TNode child: n.getChildren()) {
+					STITreeCluster pop = stack.pop();
+					childbslist.add(pop);
+					bs.or(pop.getBitSet());
+				}
+				
+				STITreeCluster cluster = new STITreeCluster();
+				cluster.setCluster((BitSet) bs.clone());
+
+				//((STINode)node).setData(new GeneTreeBitset(node.isRoot()? -2: -1));
+				stack.add(cluster);
+				node.setData(cluster);
+			}
+		}
+		stack = new Stack<STITreeCluster>();
+		List<Double> map = new ArrayList<Double>();
+		for (TNode n: st.postTraverse()) {
+			STINode node = (STINode) n;
+			if (node.isLeaf()) {
+				stack.push((STITreeCluster) node.getData());
+			} else {
+				STITreeCluster cluster = (STITreeCluster) node.getData();
+				STITreeCluster c1 = stack.pop();
+				STITreeCluster c2 = stack.pop();
+				stack.push(cluster);
+				
+				if (node.isRoot() ) {
+					continue;
+				}
+				
+				if (node.getChildCount() > 2) {
+					throw new RuntimeException("Polytomies not implemented yet. ");
+				}
+				STITreeCluster sister;
+				STITreeCluster remaining;
+				Iterator<STINode> pcit = node.getParent().getChildren().iterator();
+				STINode pc = pcit.next();
+				if ( pc == n ) pc = pcit.next(); 
+				sister = (STITreeCluster)pc.getData();
+				if (node.getParent().isRoot() && node.getParent().getChildCount() == 3) {
+					pc = pcit.next();
+					if (pc == n) pc = pcit.next(); 
+					remaining = (STITreeCluster)pc.getData();;					
+				} else {
+					remaining = ((STITreeCluster)node.getParent().getData()).complementaryCluster();
+				}
+				Quadrapartition quad = weightCalculator2.new Quadrapartition
+						(c1,  c2, sister, remaining);
+				double s = weightCalculator2.getWeight(quad);
+				map.add(s);							
+			}
+		}
+		int i = 0;
+		for (TNode n: st.postTraverse()) {
+			STINode node = (STINode) n;
+			if (!node.isLeaf() && !node.isRoot()) {
+				Double p = map.get(i);
+				node.setData(p);
+				i++;
+				Double bl = -Math.log(1.5*(1.0-p/100.0));
+				if (bl.isInfinite()) {
+					bl = 10.;
+				}
+				node.setParentDistance(bl);
+			} else {
+				node.setData(null);
+			}
+		}
+		
 	}
 
 
