2ad3ff5087ebefcd04c9115ebce03c807f34d3be
Siavash Mirarab <smirarab@gmail.com>
2015-12-16 17:29:16 -0800

    4.9.6

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -38,7 +38,7 @@ import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
 
-    protected static String _versinon = "4.9.5";
+    protected static String _versinon = "4.9.6";
 
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
