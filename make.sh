1846fb27d2ff57083cce5dd6917c370778bd4391
johng23 <yinjohn23@gmail.com>
2016-07-15 21:54:26 -0700

    added a readme for running my java test code
    Former-commit-id: cc4ecb555c92819088ff4e62098d57956571f449

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ cd main
 
 rm -f phylonet/coalescent/*.class phylonet/util/BitSet.class phylonet/tree/model/sti/STITreeCluster*.class phylonet/tree/io/NewickWriter.class
 
-javac -source 1.5  -target 1.5 -classpath ../lib/main.jar:../lib/colt.jar:../lib/JSAP-2.1.jar phylonet/util/BitSet.java phylonet/coalescent/*java phylonet/tree/model/sti/STITreeCluster.java phylonet/tree/io/NewickWriter.java
+javac -source 1.7  -target 1.7 -classpath ../lib/main.jar\;../lib/colt.jar\;../lib/JSAP-2.1.jar phylonet/util/BitSet.java phylonet/coalescent/*java phylonet/tree/model/sti/STITreeCluster.java phylonet/tree/io/NewickWriter.java
 
 jar cvfm ../astral.$version.jar ../manifest.text phylonet/util/BitSet.* phylonet/coalescent/*.* phylonet/tree/model/sti/STITreeCluster*.* phylonet/tree/io/NewickWriter.*
 
@@ -27,7 +27,7 @@ ln -s ../lib .
 ln -s ../README.md .
 ln -s ../astral.$version.jar .
 ln -s ../main/test_data .
-ln -s ../astral-tutorial.pdf .
+ln -s ../astral-tutorial.md .
 ln -s ../thesis-astral.pdf .
 cd ..
 rm -f Astral.$version.zip
