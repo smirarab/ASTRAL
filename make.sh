84f5d2991e7fbe047487d96de70ce5ba90de62d7
John Bohan Yin <johng23@comet-ln3.sdsc.edu>
2016-08-18 15:11:56 -0700

    updated
    Former-commit-id: e92ecd2b1ebe1ded17811a3b43bea7d3c324a601

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ cd main
 
 rm -f phylonet/coalescent/*.class phylonet/util/BitSet.class phylonet/tree/model/sti/STITreeCluster*.class phylonet/tree/io/NewickWriter.class
 
-javac -source 1.7  -target 1.7 -classpath ../lib/main.jar\;../lib/colt.jar\;../lib/JSAP-2.1.jar\;../lib/JOCL-0.2.0RC.jar phylonet/util/BitSet.java phylonet/coalescent/*.java phylonet/tree/model/sti/STITreeCluster.java phylonet/tree/io/NewickWriter.java
+javac -source 1.7 -target 1.7 -classpath ../lib/main.jar:../lib/colt.jar:../lib/JSAP-2.1.jar:../lib/jocl-2.0.0.jar phylonet/util/BitSet.java phylonet/coalescent/*.java phylonet/tree/model/sti/STITreeCluster.java phylonet/tree/io/NewickWriter.java
 
 jar cvfm ../astral.$version.jar ../manifest.text phylonet/util/BitSet.* phylonet/coalescent/*.* phylonet/tree/model/sti/STITreeCluster*.* phylonet/tree/io/NewickWriter.*
 
@@ -30,10 +30,3 @@ ln -s ../main/test_data .
 ln -s ../astral-tutorial.md .
 ln -s ../thesis-astral.pdf .
 cd ..
-rm -f Astral.$version.zip
-zip -r Astral.$version.zip Astral 
-
-set +x
-echo "
-Build finished successfully. You can distribute Astral.$version.zip or simply run astral.$version.jar. 
-  Note that if you are moving astral.$version.jar to some other location, you need to also move the lib directory."
