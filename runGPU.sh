e375ff86fb2019befa9bb7a0bc5a02fbd32493a0
John Bohan Yin <johng23@comet-06-26.sdsc.edu>
2016-09-09 11:18:16 -0700

    getting rid of deleted files
    Former-commit-id: 80876920702cf99d64d3d5576086e85ba416179b

--- runGPU.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd main/gputest/
-
-javac -Xlint:deprecation -cp ../../JOCL-0.2.0RC.jar GPUCall.java Main.java
-java -Djava.library.path=../../ -cp ../../JOCL-0.2.0RC.jar:. Main
-
-cd ../../
