a6397218632feac14cbbc9e45d03da6c6bb3097b
erfan <esayyari@eng.ucsd.edu>
2016-09-29 13:10:14 -0700

    4.10.11

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -39,7 +39,7 @@ import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
 
-    protected static String _versinon = "4.10.10";
+    protected static String _versinon = "4.10.11";
 
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
