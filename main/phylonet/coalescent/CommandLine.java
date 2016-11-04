3272b155f87d4e7c02b73a0d488211d9061ed347
Siavash Mirarab <smirarab@gmail.com>
2016-11-03 23:40:41 -0700

    4.10.12: update code documentation and some refactoring

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -39,7 +39,7 @@ import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
 
-    protected static String _versinon = "4.10.11";
+    protected static String _versinon = "4.10.12";
 
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
