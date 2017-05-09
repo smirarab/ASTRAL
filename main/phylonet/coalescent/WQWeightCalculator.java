c5b1f44a0b9e8e5c1e2a31952bdc4a26c8092958
chaoszhang <chz069@ucsd.edu>
2017-05-08 20:20:32 -0700

    more constant time speed-up

--- main/phylonet/coalescent/WQWeightCalculator.java
+++ main/phylonet/coalescent/WQWeightCalculator.java
@@ -53,13 +53,6 @@ class WQWeightCalculator extends AbstractWeightCalculator<Tripartition> {
 			ret *= a * b * c;
 			return ret;
 		}
-		
-		long F(int[] x, int[] y, int[] z){
-			long a = x[0], b = x[1], c = x[2], d = y[0], e = y[1], f = y[2], g = z[0], h = z[1], i = z[2];
-			return a * ( (a + e + i - 3)  * e * i + (a + f + h - 3)  * f * h )
-				 + b * ((b + d + i - 3)  * d * i + (b + f + g - 3)  * f * g )
-				 + c * ((c + d + h - 3)  * d * h + (c + e + g - 3)  * e * g );
-		}
 
 		abstract Long calculateWeight(Tripartition trip);
 
