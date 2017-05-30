3197d854054b761c7ae7f85812ec574cec0a8be6
Siavash Mirarab <smirarab@gmail.com>
2017-05-30 09:46:33 -0700

    4.11.1: change similarity matrix for unresolved

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -39,7 +39,7 @@ import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
 
-    protected static String _versinon = "4.10.13";
+    protected static String _versinon = "4.11.1";
 
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
