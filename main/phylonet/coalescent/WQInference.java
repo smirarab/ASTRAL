380a9c6952aab91ef5ea5150ce8f51e296426d37
esayyari <esayyari@ucsd.edu>
2017-07-04 12:47:06 -0700

    correcting y label of relative freq

--- main/phylonet/coalescent/WQInference.java
+++ main/phylonet/coalescent/WQInference.java
@@ -511,26 +511,28 @@ public class WQInference extends AbstractInference<Tripartition> {
 			try {
 				RscriptLines.add("#!/usr/bin/env Rscript");
 				RscriptLines.add("red='#d53e4f';orange='#1d91c0';blue='#41b6c4';colormap = c(red,orange,blue)");
-				RscriptLines.add("require(reshape2);require(ggplot2); sizes <- c(7.67,8);");
+				RscriptLines.add("require(reshape2);require(ggplot2);");
 				RscriptLines.add("dirPath = '"+options.getFreqOutputPath()+"'; filePath = paste(dirPath"
 						+ ",'/freqQuadCorrected.csv',sep=''); md<-read.csv(filePath,header=F,sep='\\t'); md$value = md$V5/md$V6;");
-				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=value,fill=V8)+"
+				RscriptLines.add("a<-length(levels(as.factor(md$V7)))*3.7; b<-4; sizes <- c(a,b);");
+				RscriptLines.add("md$V8<-reorder(md$V8,-md$value)");
+				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=value,fill=V9)+"
 						+ "geom_bar(stat='identity',color=1,width=0.8,position='dodge')+"
 						+ "theme_bw()+theme(axis.text.x=element_text(angle=90))+scale_fill_manual"
 						+ "(values=colormap,name='Topology')+geom_hline(yintercept=1/3,size=0.4,linetype=2)+"
-						+ "ylab('quartet score')+facet_wrap(~V7,scales='free_x')+scale_x_discrete(breaks=c())+xlab('')");
+						+ "ylab('relative freq.')+facet_wrap(~V7,scales='free_x')+xlab('')");
 				RscriptLines.add("pdfFile = paste(dirPath,'/relativeFreq.pdf',sep=''); ggsave(pdfFile,width = sizes[1], height= sizes[2]);");
-				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=V4,fill=V8)+"
+				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=V4,fill=V9)+"
 						+ "geom_bar(stat='identity',color=1,position='dodge')+"
 						+ "theme_bw()+theme(axis.text.x=element_text(angle=90))+scale_fill_manual"
 						+ "(values=colormap,name='Topology')+geom_hline(yintercept=1/3,size=0.4,linetype=2)+"
-						+ "ylab('quartet score')+facet_wrap(~V7,scales='free_x')+scale_x_discrete(breaks=c())+xlab('')");
+						+ "ylab('quartet score')+facet_wrap(~V7,scales='free_x')+xlab('')");
 				RscriptLines.add("pdfFile = paste(dirPath, '/quartetScore.pdf',sep=''); ggsave(pdfFile,width = sizes[1], height= sizes[2]);");
-				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=V5,fill=V8)+"
+				RscriptLines.add("ggplot(data=md)+aes(x=V8,y=V5,fill=V9)+"
 						+ "geom_bar(stat='identity',color=1,position='dodge')+"
 						+ "theme_bw()+theme(axis.text.x=element_text(angle=90))+scale_fill_manual"
 						+ "(values=colormap,name='Topology')+geom_hline(aes(yintercept=V6/3),size=0.4,linetype=2)+"
-						+ "ylab('quartet score')+facet_wrap(~V7,scales='free_x')+scale_x_discrete(breaks=c())+xlab('')");
+						+ "ylab('#genes')+facet_wrap(~V7,scales='free_x')+xlab('')");
 				RscriptLines.add("pdfFile = paste(dirPath, '/countTopology.pdf',sep=''); ggsave(pdfFile,width = sizes[1], height= sizes[2]);");
 				Files.write(freqWriter, freqWriterLines, Charset.forName("UTF-8"));
 				Files.write(Rscript, RscriptLines, Charset.forName("UTF-8"));
