4acabc51f54463a639a3f5195904592d4119bd25
chaoszhang <chz069@ucsd.edu>
2017-08-24 05:12:04 +0800

    Update CommandLine.java

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -38,7 +38,7 @@ import com.martiansoftware.jsap.Switch;
 import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
-    protected static String _versinon = "5.5.2";
+    protected static String _versinon = "5.5.3";
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
         System.err.println();
