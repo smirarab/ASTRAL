b28d49ea06aed490b361c3cda9146ec96f00dab6
Siavash Mirarab <smirarab@gmail.com>
2017-11-12 16:02:41 -0800

    5.5.8: branch length bug fix

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -38,7 +38,7 @@ import com.martiansoftware.jsap.Switch;
 import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
-    protected static String _versinon = "5.5.7";
+    protected static String _versinon = "5.5.8";
 
     private static void exitWithErr(String extraMessage, SimpleJSAP jsap) {
         System.err.println();
