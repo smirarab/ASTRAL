382a53859a5546b5677075c7c1249c585bf376db
Siavash Mirarab <smirarab@gmail.com>
2017-11-13 12:53:32 -0800

    5.5.9: -k fixed

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -38,7 +38,7 @@ import com.martiansoftware.jsap.Switch;
 import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine{
-    protected static String _versinon = "5.5.8";
+    protected static String _versinon = "5.5.9";
 
     protected static SimpleJSAP jsap;
     
