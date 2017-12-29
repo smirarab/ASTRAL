5f349d7be2480f108bba6012d71d0a4a94cbed41
chaos <chaos@LAPTOP-18PV7KBR>
2017-12-28 17:07:40 -0800

    New searching algorithm.

--- main/phylonet/coalescent/AbstractComputeMinCostTask.java
+++ main/phylonet/coalescent/AbstractComputeMinCostTask.java
@@ -6,11 +6,14 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.HashMap;
+import java.util.Random;
 
 import phylonet.coalescent.IClusterCollection.VertexPair;
 import phylonet.tree.model.Tree;
 import phylonet.tree.model.sti.STITreeCluster;
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
+import phylonet.util.BitSet;
 
 /**
  * This class implements the dynamic programming
@@ -30,8 +33,56 @@ public abstract class AbstractComputeMinCostTask<T> {
     private SpeciesMapper spm;
 
 	protected Double compute() {
+		Random rnd = new Random(System.currentTimeMillis());
+		int n = GlobalMaps.taxonIdentifier.taxonCount();
+		long[] hash1 = new long[n], hash2 = new long[n];
+		Vertex[][] arr = new Vertex[n + 1][];
+		HashMap<Long, Vertex> map = new HashMap<Long, Vertex>();
+		boolean succeed = false;
+		while (!succeed) {
+			map = new HashMap<Long, Vertex>();
+			succeed = true;
+			for (int i = 0; i < n; i++) {
+				hash1[i] = rnd.nextLong();
+				hash2[i] = rnd.nextLong();
+			}
+			for (int i = 1; i <= n; i++) {
+				if (clusters.getSubClusters(i) == null) {
+					arr[i] = new Vertex[0];
+					continue;
+				}
+				arr[i] = new Vertex[clusters.getSubClusters(i).size()];
+				int j = 0;
+				for (Vertex v: clusters.getSubClusters(i)) {
+					arr[i][j++] = v;
+				}
+			}
+			for (int i = 1; i <= n; i++) {
+				for (int j = 0; j < arr[i].length; j++) {
+					long h1 = 0, h2 = 0;
+					Vertex v = arr[i][j];
+					BitSet b = v.getCluster().getBitSet();
+					for (int k = 0; k < n; k++) {
+						if (b.get(k)) {
+							h1 += hash1[k];
+							h2 ^= hash2[k];
+						}
+					}
+					if (map.containsKey(h1)) {
+						succeed = false;
+						break;
+					}
+					map.put(h1, v);
+					v.hash1 = h1;
+					v.hash2 = h2;
+					v.clusterSize = i;
+				}
+				if (!succeed) break;
+			}
+		}
+		
 		ArrayList<VertexPair> todolist = new ArrayList<VertexPair>();
-		computeWeights(todolist);
+		computeWeights(todolist, arr, map);
 		Polytree.PTNative.compute(todolist);
 		return computeMinCost();
 	}
@@ -88,13 +139,14 @@ public abstract class AbstractComputeMinCostTask<T> {
 		}
 	}
 	
-	private void computeWeights(ArrayList<VertexPair> todolist) {
+	private void computeWeights(ArrayList<VertexPair> todolist, Vertex[][] arr, HashMap<Long, Vertex> map) {
 		if (v._done == 4) {
 			return;
 		}
 		
 		int clusterSize = v.getCluster().getClusterSize();
-
+		v.clusterResolutions = new ArrayList<VertexPair>();
+		
 		// SIA: base case for singelton clusters.
 		if (clusterSize <= 1 || spm.isSingleSP(v.getCluster().getBitSet())) {
 			v._max_score = 0;
@@ -103,40 +155,37 @@ public abstract class AbstractComputeMinCostTask<T> {
 			
 			return;
 		}
-		
-		Iterable<VertexPair> clusterResolutions;
-		containedVertecies = clusters.getContainedClusters(v);
-		
 		if (clusterSize == GlobalMaps.taxonIdentifier.taxonCount()) {
-			clusterResolutions = new ArrayList<VertexPair>();
-			Vertex v1 = null;
-			int smallestSize = 1;
-			while (v1 == null) {
-				Set<Vertex> cs = containedVertecies.getSubClusters(smallestSize);
-				if (cs.size() != 0)
-					v1 = cs.iterator().next();
-				else 
-					smallestSize++;
+			Vertex v1 = null, v2 = null;
+			for (int i = 0; i < arr[1].length; i++) {
+				v1 = arr[1][i];
+				v2 = map.get(v.hash1 - v1.hash1);
+				if (v2 != null) break;
 			}
-			for (Vertex v2: containedVertecies.getSubClusters(GlobalMaps.taxonIdentifier.taxonCount()-smallestSize))
-			{
-				if (v1.getCluster().isDisjoint(v2.getCluster())) {
-					VertexPair vp = new VertexPair(v1, v2, v);
-					((ArrayList<VertexPair>) clusterResolutions).add(vp);
-					break;
+			v.clusterResolutions.add(new VertexPair(v1, v2, v));
+		}
+		else {
+			for (int i = 1; i <= clusterSize / 2; i++) {
+				Vertex[] ar = (arr[i].length < arr[clusterSize - i].length) ? arr[i] : arr[clusterSize - i];
+				for (Vertex v1: ar) {
+					Vertex v2 = map.get(v.hash1 - v1.hash1);
+					if (v2 != null) {
+						if (v.hash2 == (v1.hash2 ^ v2.hash2)
+							&& v.clusterSize == v1.clusterSize + v2.clusterSize) {
+							if (v1.clusterSize != v2.clusterSize || v1.hash1 < v2.hash1) {
+								v.clusterResolutions.add(new VertexPair(v1, v2, v));
+							}
+						}
+					}
 				}
 			}
-			
-		} else {
-			clusterResolutions = containedVertecies.getClusterResolutions();
 		}
-		v.clusterResolutions = clusterResolutions;
-		for (VertexPair bi : clusterResolutions){
+		for (VertexPair bi : v.clusterResolutions){
 			if (clusterSize == GlobalMaps.taxonIdentifier.taxonCount()) bi.weight = defaultWeightForFullClusters();
 			else todolist.add(bi);
 			
-			newMinCostTask(bi.cluster1, containedVertecies).computeWeights(todolist);
-			newMinCostTask(bi.cluster2, containedVertecies).computeWeights(todolist);
+			newMinCostTask(bi.cluster1, containedVertecies).computeWeights(todolist, arr, map);
+			newMinCostTask(bi.cluster2, containedVertecies).computeWeights(todolist, arr, map);
 		}
 		v._done = 4;
 		return;
