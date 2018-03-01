e59ea004ac07c4e784d38880ee6c62ab8b60301d
Siavash Mirarab <smirarab@gmail.com>
2018-03-01 14:50:45 -0800

    first working hash version

--- main/phylonet/coalescent/WQClusterCollection.java
+++ main/phylonet/coalescent/WQClusterCollection.java
@@ -3,7 +3,7 @@ package phylonet.coalescent;
 import java.util.HashSet;
 
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
-
+/*
 public class WQClusterCollection extends AbstractClusterCollection{
 
 	public WQClusterCollection(int len) {
@@ -15,6 +15,24 @@ public class WQClusterCollection extends AbstractClusterCollection{
 		return new WQClusterCollection(size);
 	}
 		
+	public void printDiff(WQClusterCollection other) {
+		for (int i = 0; i< this.clusters.size(); i++) {
+			HashSet<Vertex> temp = new HashSet<Vertex>(this.clusters.get(i));
+			temp.retainAll(other.clusters.get(i));
+			System.err.println(temp);
+		}
+	}
+
+}
+
+*/
+
+public class WQClusterCollection extends HashClusterCollection{
+
+	public WQClusterCollection(int len) {
+		super(len);
+	}
+
 	public void printDiff(WQClusterCollection other) {
 		for (int i = 0; i< this.clusters.size(); i++) {
 			HashSet<Vertex> temp = new HashSet<Vertex>(this.clusters.get(i));
