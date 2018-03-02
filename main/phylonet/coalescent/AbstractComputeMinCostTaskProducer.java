ee76ae2bc030757b1159139e8bc46b4d11a324d8
Siavash Mirarab <smirarab@gmail.com>
2018-03-02 09:49:47 -0800

    update class names

--- main/phylonet/coalescent/AbstractComputeMinCostTaskProducer.java
+++ main/phylonet/coalescent/AbstractComputeMinCostTaskProducer.java
@@ -2,13 +2,13 @@ package phylonet.coalescent;
 
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
 
-public abstract class AbstractComputeMinCostTaskNoCalculations<T> extends  AbstractComputeMinCostTask{
+public abstract class AbstractComputeMinCostTaskProducer<T> extends  AbstractComputeMinCostTask{
 
 	//IClusterCollection clusters;
 
 	//IClusterCollection containedVertecies;
 	
-	public AbstractComputeMinCostTaskNoCalculations(AbstractInferenceNoCalculations<T> inference, Vertex v) {
+	public AbstractComputeMinCostTaskProducer(AbstractInferenceProducer<T> inference, Vertex v) {
 		super(inference, v);
 		//this.clusters = clusters;
 	}
