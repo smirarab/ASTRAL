5ba096eb23d5989dd9923f20e7e9d145646e60d3
Siavash Mirarab <smirarab@gmail.com>
2018-03-02 09:49:47 -0800

    update class names

--- main/phylonet/coalescent/AbstractInferenceProducer.java
+++ main/phylonet/coalescent/AbstractInferenceProducer.java
@@ -1,51 +1,28 @@
 package phylonet.coalescent;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.Stack;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import phylonet.coalescent.IClusterCollection.VertexPair;
-import phylonet.tree.model.MutableTree;
-import phylonet.tree.model.TNode;
 import phylonet.tree.model.Tree;
-import phylonet.tree.model.sti.STINode;
-import phylonet.tree.model.sti.STITree;
-import phylonet.tree.model.sti.STITreeCluster;
 import phylonet.tree.model.sti.STITreeCluster.Vertex;
-import phylonet.tree.util.Collapse;
 
-public abstract class AbstractInferenceNoCalculations<T> extends AbstractInference<T> {
+public abstract class AbstractInferenceProducer<T> extends AbstractInference<T> {
 	
 	LinkedBlockingQueue<Tripartition> queue1;
 	LinkedBlockingQueue<Iterable<VertexPair>> queue4;
-	public AbstractInferenceNoCalculations(Options options, List<Tree> trees,
+	public AbstractInferenceProducer(Options options, List<Tree> trees,
 			List<Tree> extraTrees) {
 		super(options, trees, extraTrees);
-		initDF();
 	}
 	
-	public AbstractInferenceNoCalculations(AbstractInference in) {
+	public AbstractInferenceProducer(AbstractInference in) {
 		super(in.options, in.trees, in.extraTrees);
 		this.dataCollection = in.dataCollection;
-		initDF();
 	}
 
-	private void initDF() {
-		df = new DecimalFormat();
-		df.setMaximumFractionDigits(2);
-		DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
-		dfs.setDecimalSeparator('.');
-		df.setDecimalFormatSymbols(dfs);
-	}
-
-	public abstract double scoreGeneTree(Tree scorest, boolean initialize) ;
-	
 
 	public List<Solution> inferSpeciesTree() {
 
@@ -54,12 +31,6 @@ public abstract class AbstractInferenceNoCalculations<T> extends AbstractInferen
 		}
 		return super.inferSpeciesTree();
 	}
-
-	abstract IClusterCollection newClusterCollection();
-	
-	abstract AbstractDataCollection<T> newCounter(IClusterCollection clusters);
-	
-	abstract AbstractWeightCalculatorNoCalculations<T> newWeightCalculator();
 
 	@Override
 	public Iterable<VertexPair> getClusterResolutions(Vertex v) {
@@ -110,11 +81,11 @@ public abstract class AbstractInferenceNoCalculations<T> extends AbstractInferen
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
+	
 		}
 		return clusterResolutions;
 	}
-	
+
 	/**
 	 * Sets up data structures before starting DP
 	 */
@@ -123,4 +94,12 @@ public abstract class AbstractInferenceNoCalculations<T> extends AbstractInferen
 		this.initializeWeightCalculator();
 		this.setupMisc();
 	}
+
+	public abstract double scoreGeneTree(Tree scorest, boolean initialize) ;
+
+	abstract IClusterCollection newClusterCollection();
+	
+	abstract AbstractDataCollection<T> newCounter(IClusterCollection clusters);
+	
+	abstract AbstractWeightCalculatorProducer<T> newWeightCalculator();
 }
