5475dc5a99d56c5f7f352488a86f36d96f450732
Siavash Mirarab <smirarab@gmail.com>
2018-03-08 18:05:41 -0800

    5.11.5: fixes

--- main/phylonet/coalescent/AbstractDataCollection.java
+++ main/phylonet/coalescent/AbstractDataCollection.java
@@ -25,14 +25,14 @@ public abstract class AbstractDataCollection <T> {
 	protected boolean addToClusters(STITreeCluster c, int size) {
 		
 		//String bug = "111_0_4, 111_0_0, 81_0_2, 102_0_0, 102_0_2, 102_0_3, 102_0_1, 102_0_4, 111_0_3, 111_0_2, 111_0_1, 53_0_2, 53_0_3, 53_0_4, 44_0_0, 53_0_1, 119_0_4, 119_0_0, 119_0_2, 119_0_1, 119_0_3, 44_0_3, 53_0_0, 44_0_4, 44_0_1, 44_0_2, 81_0_3, 81_0_0, 81_0_4, 81_0_1";
-		String bug = "111_0_4, 111_0_0, 116_0_1, 81_0_2, 102_0_0, 102_0_2, 102_0_3, 102_0_1, 102_0_4, 105_0_4, 105_0_1, 111_0_3, 111_0_2, 111_0_1, 116_0_2, 116_0_0, 116_0_4, 116_0_3, 53_0_2, 53_0_3, 53_0_4, 44_0_0, 53_0_1, 105_0_0, 105_0_2, 105_0_3, 119_0_4, 119_0_0, 119_0_2, 119_0_1, 119_0_3, 44_0_3, 53_0_0, 44_0_4, 44_0_1, 44_0_2, 81_0_3, 81_0_0, 81_0_4, 81_0_1";
-		String[] bugs = bug.split(", ");
-		Set<String> clusterSet = new HashSet<String>(Arrays.asList(c.getClusterLeaves()));
-		Set<String> bugSet = new HashSet<String>(Arrays.asList(bugs));	
-		if(bugSet.equals(clusterSet)){
+		//String bug = "111_0_4, 111_0_0, 116_0_1, 81_0_2, 102_0_0, 102_0_2, 102_0_3, 102_0_1, 102_0_4, 105_0_4, 105_0_1, 111_0_3, 111_0_2, 111_0_1, 116_0_2, 116_0_0, 116_0_4, 116_0_3, 53_0_2, 53_0_3, 53_0_4, 44_0_0, 53_0_1, 105_0_0, 105_0_2, 105_0_3, 119_0_4, 119_0_0, 119_0_2, 119_0_1, 119_0_3, 44_0_3, 53_0_0, 44_0_4, 44_0_1, 44_0_2, 81_0_3, 81_0_0, 81_0_4, 81_0_1";
+		//String[] bugs = bug.split(", ");
+		//Set<String> clusterSet = new HashSet<String>(Arrays.asList(c.getClusterLeaves()));
+		//Set<String> bugSet = new HashSet<String>(Arrays.asList(bugs));	
+		/*if(bugSet.equals(clusterSet)){
             System.err.println(c.toString());
             throw new RuntimeException();
-		}
+		}*/
 	
 		if (size == 0) return false;
 		Vertex nv = c.new Vertex();
