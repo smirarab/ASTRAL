e51f1715fcc264b542877dec3eec86b99bef94bf
Siavash Mirarab <smirarab@gmail.com>
2018-03-09 19:40:34 -0800

    more refactoring

---
+++ main/phylonet/coalescent/Logging.java
@@ -0,0 +1,15 @@
+package phylonet.coalescent;
+
+public class Logging {
+
+	public static long timer;
+	public static final boolean timerOn = false;
+	public static void logTimeMessage(String message) {
+		if (timerOn) {
+			System.err.println("TIME TOOK FROM LAST NOTICE " + message 
+					+" "+ (double) (System.currentTimeMillis() - timer) / 1000);
+			timer = System.currentTimeMillis();
+		}
+	}
+
+}
