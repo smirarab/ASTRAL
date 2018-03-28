0efd953cfdc13453ee512e8f939cff7a84321d25
Siavash Mirarab <smirarab@gmail.com>
2018-03-28 09:19:26 -0700

    remove unneeded imports. Update some log info

--- main/phylonet/coalescent/Matrix.java
+++ main/phylonet/coalescent/Matrix.java
@@ -1,11 +1,7 @@
 package phylonet.coalescent;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
 
 import phylonet.tree.model.Tree;
 import phylonet.tree.model.sti.STITreeCluster;
