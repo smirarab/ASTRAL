2123c75571dff834c769f9dc95a7fb52c2371d6d
Siavash Mirarab <smirarab@gmail.com>
2018-05-01 10:59:45 -0700

    5.12.6a: don't use too much memory for similarity matrix

--- main/phylonet/coalescent/CommandLine.java
+++ main/phylonet/coalescent/CommandLine.java
@@ -54,7 +54,7 @@ import com.martiansoftware.jsap.Switch;
 import com.martiansoftware.jsap.stringparsers.FileStringParser;
 
 public class CommandLine {
-	protected static String _version = "5.12.5";
+	protected static String _version = "5.12.6a";
 
 	protected static SimpleJSAP jsap;
 
