525cea1793de0327bddd9509e196a699d1b0f4a1
Siavash Mirarab <smirarab@gmail.com>
2019-03-18 23:38:44 -0700

    5.14.0: merge with the master version

--- compile_native_c_code.sh
+++ compile_native_c_code.sh
@@ -1,5 +1,11 @@
-for word in `java -XshowSettings:properties -version 2>&1 | grep java.home`; do test -d ${word}/include/ && DIR=${word}/include/ || DIR=${word}/../include/; done && for word in `ls -d ${DIR}*/`; do SUBDIR=${word}; done || (( DIR=. && SUBDIR=. ))
-test `uname` == "Darwin" && ext='dylib' || ext='so'
-if [ -z $JAVA_HOME ]; then DIR2=.; SUBDIR2=.; else DIR2=$JAVA_HOME; for word in `ls -d ${DIR2}/*/`; do SUBDIR2=${word}; done || (( DIR2=. && SUBDIR2=. )); fi
-mv lib/libAstral.${ext} lib/libAstral.${ext}.old || echo "no default library found"
-g++ -std=c++11 -I"$DIR" -I"$SUBDIR" -I"$DIR2" -I"$SUBDIR2" -march=native -Ofast -fPIC -o lib/libAstral.${ext} -shared main/phylonet_coalescent_Polytree_PTNative.cpp || mv lib/libAstral.${ext}.old lib/libAstral.${ext} || echo "Native library not compiled and default version not found."
+for word in `java -XshowSettings:properties -version 2>&1 | grep java.home`; do DIR=${word}/../include/; done && for word in `ls -d ${DIR}*/`; do SUBDIR=${word}; done || (( DIR=. && SUBDIR=. ))
+if [ -z ${SUBDIR} ]; then DIR=.; SUBDIR=.; fi
+if [ -z $JAVA_HOME ]; then DIR2=.; SUBDIR2=.; else DIR2=$JAVA_HOME/../include/; for word in `ls -d ${DIR2}*/`; do SUBDIR2=${word}; done || (( DIR2=. && SUBDIR2=. )); fi
+if [ -z ${SUBDIR2} ]; then DIR2=.; SUBDIR2=.; fi
+for word in `java -XshowSettings:properties -version 2>&1 | grep java.home`; do DIR3=${word}/include/; done && for word in `ls -d ${DIR3}*/`; do SUBDIR3=${word}; done || (( DIR3=. && SUBDIR3=. ))
+if [ -z ${SUBDIR3} ]; then DIR3=.; SUBDIR3=.; fi
+if [ -z $JAVA_HOME ]; then DIR4=.; SUBDIR4=.; else DIR4=$JAVA_HOME/include/; for word in `ls -d ${DIR4}*/`; do SUBDIR4=${word}; done || (( DIR4=. && SUBDIR4=. )); fi
+if [ -z ${SUBDIR4} ]; then DIR4=.; SUBDIR4=.; fi
+if [ `uname -s` == Darwin ]; then LIBNAME=libAstral.dylib; else LIBNAME=libAstral.so; fi
+mv lib/${LIBNAME} lib/${LIBNAME}.old || echo "no default library found"
+g++ -std=c++11 -I"$DIR" -I"$SUBDIR" -I"$DIR2" -I"$SUBDIR2" -I"$DIR3" -I"$SUBDIR3" -I"$DIR4" -I"$SUBDIR4" -march=native -Ofast -fPIC -o lib/${LIBNAME} -shared main/phylonet_coalescent_Polytree_PTNative.cpp || icc -std=c++11 -I"$DIR" -I"$SUBDIR" -I"$DIR2" -I"$SUBDIR2" -I"$DIR3" -I"$SUBDIR3" -I"$DIR4" -I"$SUBDIR4" -march=native -Ofast -fPIC -o lib/${LIBNAME} -shared main/phylonet_coalescent_Polytree_PTNative.cpp || mv lib/${LIBNAME}.old lib/${LIBNAME} || echo "Native library not compiled and default version not found." 
