f2d8ada1fd69d0dfd069f5e815b8e7b549d8baec
chaoszhang <chz069@ucsd.edu>
2019-03-18 19:06:04 -0700

    add icc support

---
+++ compile_native_c_code.sh
@@ -0,0 +1,7 @@
+for word in `java -XshowSettings:properties -version 2>&1 | grep java.home`; do DIR=${word}/../include/; done && for word in `ls -d ${DIR}*/`; do SUBDIR=${word}; done || (( DIR=. && SUBDIR=. ))
+if [ -z $JAVA_HOME ]; then DIR2=.; SUBDIR2=.; else DIR2=$JAVA_HOME; for word in `ls -d ${DIR2}/*/`; do SUBDIR2=${word}; done || (( DIR2=. && SUBDIR2=. )); fi
+for word in `java -XshowSettings:properties -version 2>&1 | grep java.home`; do DIR3=${word}/include/; done && for word in `ls -d ${DIR3}*/`; do SUBDIR3=${word}; done || (( DIR3=. && SUBDIR3=. ))
+if [ -z $JAVA_HOME ]; then DIR4=.; SUBDIR4=.; else DIR4=$JAVA_HOME; for word in `ls -d ${DIR4}/`; do SUBDIR4=${word}; done || (( DIR4=. && SUBDIR4=. )); fi
+if [ `uname -s` == Darwin ]; then LIBNAME=libAstral.dylib; else LIBNAME=libAstral.so; fi
+mv lib/${LIBNAME} lib/${LIBNAME}.old || echo "no default library found"
+g++ -std=c++11 -I"$DIR" -I"$SUBDIR" -I"$DIR2" -I"$SUBDIR2" -I"$DIR3" -I"$SUBDIR3" -I"$DIR4" -I"$SUBDIR4" -march=native -Ofast -fPIC -o lib/${LIBNAME} -shared main/phylonet_coalescent_Polytree_PTNative.cpp || icc -std=c++11 -I"$DIR" -I"$SUBDIR" -I"$DIR2" -I"$SUBDIR2" -I"$DIR3" -I"$SUBDIR3" -I"$DIR4" -I"$SUBDIR4" -march=native -Ofast -fPIC -o lib/${LIBNAME} -shared main/phylonet_coalescent_Polytree_PTNative.cpp || mv lib/${LIBNAME}.old lib/${LIBNAME} || echo "Native library not compiled and default version not found."
